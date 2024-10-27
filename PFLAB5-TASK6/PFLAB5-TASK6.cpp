2787a56c91cbff509240fd87f577ee2140c2536c
Hamna-byte <yousafsaadi564@gmail.com>
2024-10-27 21:40:12 +0000

    Add project files.

---
+++ PFLAB5-TASK6/PFLAB5-TASK6.cpp
@@ -0,0 +1,13 @@
+// PFLAB5-TASK6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n=5;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "cube of the number "<<i<< " is:" <<i * i * i << endl;
+	}
+}
